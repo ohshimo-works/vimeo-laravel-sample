54a9359a1926e18c79fe488ea591dec270740f19
ohshimo <ohshimo@gmail.com>
2025-01-22 18:14:30 +0900

    Fortifyまだわからないので、適当にログイン画面

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,7 +3,11 @@
 @section("title", "ログイン")
 
 @section("content")
-    <form action="{{ route("login")}}">
-        
+    <form action="{{ route("login")}}" method="POST">
+        <label for="email">email</label>
+        <input type="email" name="email" id="email" placeholder="ex) sample@example.com" value="{{ old("email") }}">
+        <label for="password">パスワード</label>
+        <input type="password" name="password" id="password">
+        <input type="submit" value="Login">
     </form>
 @endsection
