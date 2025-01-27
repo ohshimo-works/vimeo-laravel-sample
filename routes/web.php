019301ed97b1dae3cfb4a9bbb90a4b418e0ea75a
ohshimo <ohshimo@gmail.com>
2025-01-27 23:41:42 +0900

    add routing

--- routes/web.php
+++ routes/web.php
@@ -7,5 +7,7 @@ Route::controller(ViemoController::class)->middleware("auth")->group(function(){
     Route::get("/", "index")->name("vimeo.index");
     Route::post("/", "upload")->name("vimeo.upload");
     Route::get("/vimeo", "list")->name("vimeo.list");
-    Route::get("/vimeo/{id}", "detail")->name("vimeo.detail");
+    Route::get("/vimeo/{item}", "detail")->name("vimeo.detail");
+    Route::put("/vimeo/{item}", "replace")->name("vimeo.replace");
+    Route::delete("/vimeo/{item}", "delete")->name("vimeo.delete");
 });
